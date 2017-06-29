bdb6f71bdb42a3e5cc95ba002740cadef869d6a4
fbora <florin.bora@gmail.com>
2017-06-29 00:21:30 -0400

    added state as place holder parameter

--- sin_lstm.py
+++ sin_lstm.py
@@ -25,23 +25,30 @@ def sin_signal():
 
 class lstm_model():
     def __init__(self, size_x, size_y, num_units=32, num_layers=3, keep_prob=0.5):
-        # def single_unit():
-        #     return rnn.DropoutWrapper(
-        #         rnn.LSTMCell(num_units), output_keep_prob=keep_prob)
 
         def single_unit():
             return rnn.LSTMCell(num_units)
 
         self.graph = tf.Graph()
         with self.graph.as_default():
+            self.state_shape = [num_layers, 2, 1, num_units]
+
             '''input place holders'''
             self.X = tf.placeholder(tf.float32, [None, size_x], name='X')
             self.y = tf.placeholder(tf.float32, [None, size_y], name='y')
+            self.init_state = tf.placeholder(tf.float32, self.state_shape, name='init_state')
+
+            '''cell and cell state definitions'''
+            cell = rnn.MultiRNNCell([single_unit() for _ in range(num_layers)])
+            unstacked = tf.unstack(self.init_state, axis=0)
+            init_state_tuple = tuple([rnn.LSTMStateTuple(
+                unstacked[idx][0], unstacked[idx][1]) for idx in range(num_layers)])
 
             '''network'''
-            cell = rnn.MultiRNNCell([single_unit() for _ in range(num_layers)])
             X = tf.expand_dims(self.X, -1)
-            val, state = tf.nn.dynamic_rnn(cell, X, time_major=True, dtype=tf.float32)
+            val, state = tf.nn.dynamic_rnn(cell, X, time_major=True,
+                initial_state=init_state_tuple, dtype=tf.float32)
+            self.state = tf.identity(state, name='state')
             val = tf.transpose(val, [1, 0, 2])
             last = tf.gather(val, int(val.get_shape()[0])-1)
             weights = tf.Variable(tf.truncated_normal([num_units, size_y], 0.0, 1.0), name='weights')
@@ -71,7 +78,6 @@ class lstm_regressor():
         num_cols = 1 if len(df_shape)<2 else df_shape[1]
         return num_rows, num_cols
 
-
     def train(self, X_train, y_train, iterations):
         train_pts, size_x = lstm_regressor.get_shape(X_train)
         train_pts, size_y = lstm_regressor.get_shape(y_train)
@@ -82,11 +88,13 @@ class lstm_regressor():
             saver = tf.train.Saver()
             feed_dict={
                 model.X: X_train.values.reshape(-1, size_x),
-                model.y: y_train.values.reshape(-1, size_y)
+                model.y: y_train.values.reshape(-1, size_y),
+                model.init_state: np.zeros(model.state_shape)
             }
 
             for step in range(iterations):
-                _, loss = sess.run([model.train_op, model.loss], feed_dict=feed_dict)
+                _, loss, state = sess.run([model.train_op, model.loss, model.state], feed_dict=feed_dict)
+                feed_dict[model.init_state] = state
                 if step%100==0:
                     print('step={}, loss={}'.format(step, loss))
             saver.save(sess, './check_pts/lstm')
@@ -103,7 +111,15 @@ class lstm_regressor():
                 saver.restore(sess, './check_pts/lstm')
                 X = graph.get_tensor_by_name('X:0')
                 y_tf = graph.get_tensor_by_name('predicted_y:0')
-                y_np = sess.run(y_tf, feed_dict={X: X_np})
+                init_state_tf = graph.get_tensor_by_name('init_state:0')
+                state_tf = graph.get_tensor_by_name('state:0')
+                init_state_np= np.zeros(init_state_tf.shape)
+
+                # dummy call to get a new state
+                y_dummy, state_dummy = sess.run([y_tf, state_tf],
+                    feed_dict={X: X_np[:1, :], init_state_tf: init_state_np})
+
+                y_np = sess.run(y_tf, feed_dict={X: X_np, init_state_tf: state_dummy})
                 return y_np.reshape(test_pts)
 
 
@@ -115,7 +131,7 @@ def main():
     y_predicted = regressor.predict(test_data.X)
     test_data['y_predicted'] = y_predicted
 
-    test_data[['y', 'y_predicted']].plot()
+    test_data[['y', 'y_predicted']].plot(style=['og', 'or'])
 
 if __name__ == '__main__':
     main()
